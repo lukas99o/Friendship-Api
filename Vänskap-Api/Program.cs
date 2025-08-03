8721253da407950021acf177f21da39f7080d568
Lukas Olsson <Lukas99O@hotmail.com>
2025-08-03 12:01:27 +0200

    Successfully deploying postgresql database on railway

--- Vänskap-Api/Program.cs
+++ Vänskap-Api/Program.cs
@@ -64,7 +64,7 @@ namespace Vänskap_Api
             });
 
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+                options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
             builder.Services.AddIdentity<ApplicationUser, IdentityRole>().
                 AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 
